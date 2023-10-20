58372f9c2aaec4b6b90912f536fd07f346e17d3a
anushka3107-bit <bhardwajanushka180@gmail.com>
2023-10-20 23:39:03 +0530

    first commit

--- src/Component/AfterSignIn/SearchBar.js
+++ src/Component/AfterSignIn/SearchBar.js
@@ -5,7 +5,6 @@ import { BsSun } from "react-icons/bs";
 import { motion } from "framer-motion";
 
 const SearchBar = () => {
- 
   return (
     <>
       <div className="bg-cSearchBar flex justify-between w-full h-20 shadow-[0px_5px_20px_rgba(0,0,0,0.15)] ">
@@ -25,7 +24,6 @@ const SearchBar = () => {
           <div className=" pl-5 text-lg font-semibold">
             <button
               type="button"
-              onClick={openModal}
               className="px-2 py-2 rounded-3xl font-bold  text-white hover:text-white hover:bg-[#f8777d] "
             >
               Create Pin
